6818d37385cc8694bae9ac4037dccafebb951f5c
Karolayne <karolaynedsa16@gmail.com>
2024-11-11 16:11:43 -0300

    first commit

---
+++ sportstock/src/test/java/br/univille/sportstock/SportStock.java
@@ -0,0 +1,13 @@
+package br.univille.sportstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportStock {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
